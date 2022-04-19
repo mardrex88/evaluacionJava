9ff43f419618df34194f8bc4cf00a6395168c7d5
Marlon Vargas <mardrex88@gmail.com>
2022-04-18 21:44:58 -0500

    Se agrega las solucion al ejercicio 9

--- src/main/java/ejercicio10/Ejercicio10.java
+++ src/main/java/ejercicio10/Ejercicio10.java
@@ -1,4 +1,9 @@
 package ejercicio10;
 
 public class Ejercicio10 {
+
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/ejercicio9/Ejercicio9.java
+++ src/main/java/ejercicio9/Ejercicio9.java
@@ -3,6 +3,13 @@ package ejercicio9;
 public class Ejercicio9 {
 
     public static void main(String[] args) {
+        String text =  "La sonrisa sera la mejor arma contra la tristeza";
+        String newText;
+        System.out.println(text);
+
+        newText =  text.replace("a","s");
+        System.out.println(newText.concat(" <= esta fue la tarea"));
+
 
     }
 }
