a49b7fb87596a2488ddf96d008366ee4eb3e76d7
Marlon Vargas <mardrex88@gmail.com>
2022-04-18 21:29:57 -0500

    Se agrega las solucion al ejercicio 8

--- src/main/java/ejercicio8/Ejercicio8.java
+++ src/main/java/ejercicio8/Ejercicio8.java
@@ -1,4 +1,29 @@
 package ejercicio8;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        String text;
+
+        System.out.println("Por favor ingrese un dia de la semana");
+        text = leer.nextLine();
+
+        switch (text){
+            case "lunes":
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "viernes":
+                System.out.println("Es un dia laboral");
+                break;
+            case "Sabado":
+            case " domingo":
+                System.out.println("no es un dia laboral");
+        }
+
+    }
 }
